2cb1e4139fcc31e969c24d50222ee01614b3becb
Mikayla Morrow <57641878+mikaylamorrow@users.noreply.github.com>
2022-05-25 20:19:56 -0500

    Update aes.java

--- aes.java
+++ aes.java
@@ -1,1 +1,13 @@
+package aes;
 
+import java.util.ArrayList;
+
+public class Aes {
+    
+    public static ArrayList<int[][]> keys = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        Encrypt("SecretMessage123", "notapassword1234");      
+    }
+  
+}
